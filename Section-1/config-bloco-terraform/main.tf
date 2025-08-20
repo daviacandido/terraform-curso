6f683fc1faf6509ec2768b923fe831ea7c9cd72e
Davi A Candido <daviaraujocandido@gmail.com>
2025-08-20 17:12:51 -0300

    subindo o s3 na aws via terraform apply e destruindo via terraform destroy (usando plan nos dois casos)

--- Section-1/config-bloco-terraform/main.tf
+++ Section-1/config-bloco-terraform/main.tf
@@ -12,6 +12,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1" # Configura a região padrão para os recursos AWS.
+  
   default_tags {
     tags = {
       owner      = "daviacandido" # Define uma tag padrão para todos os recursos criados, indicando o ambiente de desenvolvimento.
