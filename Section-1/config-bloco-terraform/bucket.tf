05e2a9526a5d5fa8a38c6d54a4e4556c918b3129
Davi A Candido <daviaraujocandido@gmail.com>
2025-08-20 16:57:14 -0300

    criando o arquivo do bucket s3, rodando terraform init, fmt e validate

---
+++ Section-1/config-bloco-terraform/bucket.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "davicandido-fterraform-curso" # Nome do bucket S3 a ser criado.
+}
--- Section-1/config-bloco-terraform/main.tf
+++ Section-1/config-bloco-terraform/main.tf
@@ -1,17 +1,21 @@
 terraform {
-    required_version = "~= 1.0.0" # Define a versão mínima do Terraform necessária para este código.
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws" # Especifica o provedor AWS e sua origem.
-            version = "1.0"        # Define a versão do provedor AWS a ser utilizada.
-        }
+  required_version = ">= 1.9.0" # Define a versão mínima do Terraform necessária para este código.
 
-        azurerm = {
-            source  = "hashicorp/azurerm" # Especifica o provedor Azure e sua origem.
-            version = "2.0"           # Define a versão do provedor Azure a ser utilizada.
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" # Especifica o provedor AWS e sua origem.
+      version = "6.9.0"         # Define a versão do provedor AWS a ser utilizada.
     }
+  }
+}
 
-    backend "s3" {
+
+provider "aws" {
+  region = "us-east-1" # Configura a região padrão para os recursos AWS.
+  default_tags {
+    tags = {
+      owner      = "daviacandido" # Define uma tag padrão para todos os recursos criados, indicando o ambiente de desenvolvimento.
+      managed-by = "terraform"    # Indica que os recursos são gerenciados pelo Terraform.
     }
+  }
 }
