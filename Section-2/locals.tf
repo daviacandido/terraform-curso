087069922a526cb3b7b395f662d25fe559ac69af
Davi A Candido <daviaraujocandido@gmail.com>
2025-08-21 12:02:54 -0300

    terraform fmt

--- Section-2/locals.tf
+++ Section-2/locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags = {
-    owner       = "daviacandido" # Substitua pelo seu nome ou equipe responsável
-    managed_by = "Terraform" # Indica que os recursos são gerenciados pelo Terraform
+    owner      = "daviacandido" # Substitua pelo seu nome ou equipe responsável
+    managed_by = "Terraform"    # Indica que os recursos são gerenciados pelo Terraform
   }
 }
--- Section-2/outputs.tf
+++ Section-2/outputs.tf
@@ -1,10 +1,10 @@
 output "storage_account_id" {
-    description = "ID da conta de armazenamento criada"
-    value       = azurerm_storage_account.storage_account.id
+  description = "ID da conta de armazenamento criada"
+  value       = azurerm_storage_account.storage_account.id
 }
 
 output "sa_primary_access_key" {
-    description = "Chave de acesso primária da conta de armazenamento"
-    value       = azurerm_storage_account.storage_account.primary_access_key
-    sensitive   = true
+  description = "Chave de acesso primária da conta de armazenamento"
+  value       = azurerm_storage_account.storage_account.primary_access_key
+  sensitive   = true
 }
