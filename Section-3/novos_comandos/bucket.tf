028cb61547c762beb35930590860fa177f0307f7
Davi A Candido <daviaraujocandido@gmail.com>
2025-08-22 15:16:31 -0300

    Estudo sobre comandos terraform show / state / import / refresh e terraform init -reconfigure / -migrate-state / -backend-config

---
+++ Section-3/novos_comandos/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "daviacandido-fterraform-commands-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "daviacandido-fterraform-commands-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "daviacandido-fterraform-commands-3"
+}
