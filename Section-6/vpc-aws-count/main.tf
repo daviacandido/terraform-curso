a9e1e311a0e4b066ed94c5479fbcb2554fbed71a
Davi A Candido <daviaraujocandido@gmail.com>
2025-08-25 11:42:45 -0300

    estudando sobre count

---
+++ Section-6/vpc-aws-count/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "daviacandido-remote-state-bucket"
+    key    = "count/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" // Região onde o bucket S3 será criado
+
+  default_tags {
+    tags = {
+      owner      = "daviacandido"
+      managed-by = "terraform"
+    }
+  }
+}
+
